d4088607ca4d2e8662fde01167ad9c21e72a31b9
jonathan.olabrie@gmail.com <jonathan.olabrie@gmail.com>
2020-06-03 18:50:23 -0300

    premier commit

---
+++ js/HomePage2.js
@@ -0,0 +1,8 @@
+function toggleText() {
+    var text = document.getElementById("contentEnglish");
+    if (text.style.display === "none") {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
